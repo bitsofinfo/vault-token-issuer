bbbf1cb2e1779aaa0e2273d3250d909060776e62
bitsofinfo <bitsofinfo.g@gmail.com>
2019-11-03 15:43:41 -0500

    embed spa, new opts

--- auth/vault.go
+++ auth/vault.go
@@ -2,13 +2,13 @@ package auth
 
 import "net/http"
 
-// Interface for vault authenticators
+// VaultAuthenticator ... Interface for vault authenticators
 type VaultAuthenticator interface {
 	Auth(credential VaultCredentials) (string, error)
 	GetCredentials(req *http.Request) (VaultCredentials, error)
 }
 
-// Interface for credentials for vault
+// VaultCredentials ... Interface for credentials for vault
 type VaultCredentials interface {
 	Get() (interface{}, error)
 }
